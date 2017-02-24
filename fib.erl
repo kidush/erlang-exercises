83f24b7e64e1633c58db774b90eb4492cfbf64aa
Thiago F. Lins <thiago.evandson@gmail.com>
2017-02-24 16:33:48 -0300

    adding step-by-step on fibonacci example

--- fib.erl
+++ fib.erl
@@ -1,6 +1,13 @@
 -module (fib).
 -export ([fib/1]).
 
+% STEP-BY-STEP for fib(4) 
+%  fib(4)
+%  = fib(3) + fib(2)
+%  = (fib(2) + fib(1)) + 1
+%  = (1 + 1) + 1 
+%  = 3
+
 fib(0) ->
     0;
 fib(1) ->
@@ -8,4 +15,4 @@ fib(1) ->
 fib(2) ->
     1;
 fib(N) ->
-    fib(N - 1) + fib(N -2).
+    fib(N - 1) + fib(N - 2).
